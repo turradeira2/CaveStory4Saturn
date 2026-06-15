36e0c0ea405aa3102acf4fadb8ffca4cf417e552
Tiago Matos <tiagaomatos@hotmail.com>
2026-06-15 23:40:00 +0100

    Player test object

--- src/objects/player.cxx
+++ src/objects/player.cxx
@@ -4,3 +4,9 @@ using namespace SRL::Types;
 using namespace SRL::Math;
 using namespace SRL::Input;
 
+void LoadSprite()
+{
+    SRL::Bitmap::TGA* quote = new SRL::Bitmap::TGA("QUOTE.TGA");
+    int32_t quoteF1 = SRL::VDP1::TryLoadTexture(quote);
+    delete quote;
+}
